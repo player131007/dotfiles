51961bc4c49971deea39c557fdde63af851eb2d1
Lương Việt Hoàng <tcm4095@gmail.com>
2023-07-09 11:36:37 +0700

    Use exa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     fish_config theme choose "Rosé Pine"
     function mark_prompt_start --on-event fish_prompt
-	echo -en "\e]133;A\e\\"
+	    echo -en "\e]133;A\e\\"
     end
+    abbr --add ls exa -F --icons --header
+    abbr --add ll exa -F --icons --header -l
 end
