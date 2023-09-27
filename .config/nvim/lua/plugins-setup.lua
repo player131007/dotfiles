b86e0d69e006f2ae1a88ee8af5d5708c903a9eb2
Lương Việt Hoàng <tcm4095@gmail.com>
2023-09-27 21:25:23 +0700

    plugin changes
    remove telescope
    install javascript treesitter for ags

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -79,7 +79,7 @@ require('lazy').setup({
                 filetype = "hypr",
             }
             require('nvim-treesitter.configs').setup {
-                ensure_installed = { 'cpp', 'lua', 'nix', 'hypr' },
+                ensure_installed = { 'cpp', 'lua', 'nix', 'hypr', 'javascript' },
                 highlight = {
                     enable = true
                 }
@@ -91,28 +91,6 @@ require('lazy').setup({
         dependencies = {
             'nvim-treesitter'
         }
-    },
-    {
-        'nvim-telescope/telescope.nvim',
-        version = '*',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build = 'make'
-            }
-        },
-        cmd = 'Telescope',
-        keys = {
-            { '<leader>ff', '<cmd>Telescope find_files<CR>' },
-            { '<leader>fs', '<cmd>Telescope live_grep<CR>' },
-            { '<leader>fb', '<cmd>Telescope buffers<CR>' },
-            { '<leader>fh', '<cmd>Telescope help_tags<CR>' }
-        },
-        config = function(_, opts)
-            require('telescope').setup(opts)
-            require('telescope').load_extension('fzf')
-        end
     },
     {
         url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
