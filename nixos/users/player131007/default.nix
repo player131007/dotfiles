ad13a6fc7d67cfa640668d81db15da323c81e356
Lương Việt Hoàng <tcm4095@gmail.com>
2023-11-23 09:31:05 +0700

    manage themes with home-manager

--- nixos/users/player131007/default.nix
+++ nixos/users/player131007/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 with builtins;
 let
     fileNames = filter (name: name != ".config") (attrNames (readDir ./home));
@@ -8,5 +8,23 @@ in
     programs.home-manager.enable = true;
     home.file = lib.genAttrs fileNames (name: { source = ./home + "/${name}"; });
     xdg.configFile = lib.genAttrs configFileNames (name: { source = ./home/.config + "/${name}"; });
+
+    gtk.enable = true;
+    gtk.theme = {
+        package = pkgs.rose-pine-gtk-theme;
+        name = "rose-pine";
+    };
+    gtk.iconTheme = {
+        package = pkgs.rose-pine-icon-theme;
+        name = "rose-pine";
+    };
+    gtk.gtk4.extraCss = readFile "${pkgs.rose-pine-gtk-theme}/share/themes/rose-pine/gtk-4.0/gtk.css";
+
+    home.pointerCursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 24;
+    };
+
     home.stateVersion = "23.05";
 }
