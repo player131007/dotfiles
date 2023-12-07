ae6888e4dc58832ec0abafe03366a6b76b003096
Lương Việt Hoàng <tcm4095@gmail.com>
2023-12-07 15:02:14 +0700

    refactoring

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -59,47 +59,43 @@
         };
     };
 
-    fileSystems."/" = {
-        device = "none";
-        fsType = "tmpfs";
-        options = [ "defaults" "size=2G" "mode=755" ];
-    };
-
-    fileSystems."/persist" = {
-        label = "nixos";
-        fsType = "btrfs";
-        options = [ "compress=zstd:5" "noatime" "subvol=persist" ];
-        neededForBoot = true;
-    };
-
-    fileSystems."/nix" = {
-        label = "nixos";
-        fsType = "btrfs";
-        options = [ "compress=zstd:5" "noatime" "subvol=nix" ];
-    };
-
-    fileSystems."/var/log" = {
-        label = "nixos";
-        fsType = "btrfs";
-        options = [ "compress=zstd:5" "noatime" "subvol=log" ];
-        neededForBoot = true;
-    };
-
-    fileSystems."/boot" = {
-        label = "nixos";
-        fsType = "btrfs";
-        options = [ "compress=zstd:5" "noatime" "subvol=boot" ];
-    };
-
-    fileSystems."/home" = {
-        label = "nixos";
-        fsType = "btrfs";
-        options = [ "compress=zstd:5" "noatime" "subvol=home" ];
-    };
-
-    fileSystems."/efi" = {
-        label = "ESP";
-        fsType = "vfat";
+    fileSystems = {
+        "/" = {
+            device = "none";
+            fsType = "tmpfs";
+            options = [ "defaults" "size=2G" "mode=755" ];
+        };
+        "/persist" = {
+            label = "nixos";
+            fsType = "btrfs";
+            options = [ "compress=zstd:5" "noatime" "subvol=persist" ];
+            neededForBoot = true;
+        };
+        "/nix" = {
+            label = "nixos";
+            fsType = "btrfs";
+            options = [ "compress=zstd:5" "noatime" "subvol=nix" ];
+        };
+        "/var/log" = {
+            label = "nixos";
+            fsType = "btrfs";
+            options = [ "compress=zstd:5" "noatime" "subvol=log" ];
+            neededForBoot = true;
+        };
+        "/boot" = {
+            label = "nixos";
+            fsType = "btrfs";
+            options = [ "compress=zstd:5" "noatime" "subvol=boot" ];
+        };
+        "/home" = {
+            label = "nixos";
+            fsType = "btrfs";
+            options = [ "compress=zstd:5" "noatime" "subvol=home" ];
+        };
+        "/efi" = {
+            label = "ESP";
+            fsType = "vfat";
+        };
     };
 
     environment.persistence."/persist" = {
