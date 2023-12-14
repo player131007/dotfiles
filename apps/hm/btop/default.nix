8eb71f0f122ecdd3ab074d8ec421233328cace07
Lương Việt Hoàng <tcm4095@gmail.com>
2023-12-14 09:53:20 +0700

    theme btop with base16
    theme btop with base16

--- apps/hm/btop/default.nix
+++ apps/hm/btop/default.nix
@@ -1,6 +1,13 @@
+{ config, ... }:
 {
     programs.btop.enable = true;
+
+    xdg.configFile."btop/themes/colors.theme".source = config.scheme {
+        template = ./btop.mustache;
+        extension = ".theme";
+    };
     programs.btop.settings = {
+        color_theme = "${config.home.homeDirectory}/.config/btop/themes/colors.theme";
         theme_background = false;
         truecolor = true;
         update_ms = 1000;
