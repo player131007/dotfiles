c75fb7bd9e2abbe881e2389cc373fb76691ba1df
Lương Việt Hoàng <tcm4095@gmail.com>
2024-02-25 19:50:51 +0700

    big root

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -57,7 +57,7 @@
         "/" = {
             device = "none";
             fsType = "tmpfs";
-            options = [ "defaults" "size=2G" "mode=755" ];
+            options = [ "defaults" "size=8G" "mode=755" ];
         };
         "/persist" = {
             label = "nixos";
