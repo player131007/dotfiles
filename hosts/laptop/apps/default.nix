167b2e33ce0299e74a74b1afc60381225c9777f7
Lương Việt Hoàng <tcm4095@gmail.com>
2024-02-26 22:21:56 +0700

    replace command-not-found with nix-index

--- hosts/laptop/apps/default.nix
+++ hosts/laptop/apps/default.nix
@@ -6,6 +6,9 @@
         ./starship
     ];
 
+    programs.command-not-found.enable = false;
+    programs.nix-index.enable = true;
+
     programs.fish.enable = true;
     programs.hyprland.enable = true;
     programs.ssh.startAgent = true;
