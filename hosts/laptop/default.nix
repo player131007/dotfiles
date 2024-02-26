player131007/dotfiles

commit be7b640631141a0b653fc16451c684c38d24d1c5

replace channels with flakes

hosts/laptop/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, host, ... }:
+{ pkgs, inputs, ... }:
 {
     imports = [
         ./hardware-configuration.nix
@@ -6,12 +6,10 @@
         ./apps
     ];
 
-    nix.registry.nixpkgs.to = {
-        type = "github";
-        owner = "NixOS";
-        repo = "nixpkgs";
-        ref = "nixos-unstable";
-    };
+    nix.registry.nixpkgs.flake = inputs.nixpkgs;
+    nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
+    nix.channel.enable = false;
+
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nixpkgs.config = {
         allowUnfree = true;
@@ -31,7 +29,7 @@
         };
     };
 
-    networking.hostName = host;
+    networking.hostName = "laptop";
     networking.networkmanager.enable = true;
     networking.networkmanager.insertNameservers = [
         "1.1.1.1"

flake.nix
@@ -31,7 +31,6 @@
         nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
             specialArgs = {
                 inherit inputs;
-                host = "laptop";
             };
             modules = [
                 ./hosts/laptop

commit 51da98c6a721f8f6e24978491049bc55b7984c79

fixes lookup paths not found which is caused by be7b640

hosts/laptop/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, config, ... }:
 {
     imports = [
         ./hardware-configuration.nix
@@ -9,6 +9,9 @@
     nix.registry.nixpkgs.flake = inputs.nixpkgs;
     nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
     nix.channel.enable = false;
+
+    # here because `nix.channel.enable = false` would set this to nothing
+    nix.settings.nix-path = config.nix.nixPath;
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nixpkgs.config = {