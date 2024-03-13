5ade4dcd355080158f4fe6c8606c132fbcdbb1a5
Lương Việt Hoàng <tcm4095@gmail.com>
2024-03-14 00:18:12 +0700

    remove swaylock

--- hosts/laptop/apps/default.nix
+++ hosts/laptop/apps/default.nix
@@ -1,7 +1,6 @@
 {
     imports = [
         ./eza
-        ./swaylock
         ./starship
     ];
 
--- hosts/laptop/apps/swaylock/default.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs, ... }: {
-    environment.systemPackages = [ pkgs.swaylock ];
-    security.pam.services.swaylock = {};
-}
