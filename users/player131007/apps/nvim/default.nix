23fd6118339b9024c203ffc34223038d15d70ada
Lương Việt Hoàng <tcm4095@gmail.com>
2024-03-26 21:03:59 +0700

    fix nixvim

--- users/player131007/apps/nvim/default.nix
+++ users/player131007/apps/nvim/default.nix
@@ -112,22 +112,20 @@
                 ];
             };
 
-            nvim-cmp = {
+            cmp = {
                 enable = true;
-                snippet.expand = "luasnip";
-                mappingPresets = [ "insert" ];
-                sources = [
-                    { name = "luasnip"; }
-                    { name = "nvim_lsp"; }
-                    { name = "nvim_lsp_signature_help"; }
-                ];
-                mapping = {
-                    "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-                    "<C-f>" = "cmp.mapping.scroll_docs(4)";
-                    "<C-e>" = "cmp.mapping.abort()";
-                    "<Tab>" = {
-                        modes = [ "i" "s" ];
-                        action = ''
+                settings = {
+                    snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+                    sources = [
+                        { name = "luasnip"; }
+                        { name = "nvim_lsp"; }
+                        { name = "nvim_lsp_signature_help"; }
+                    ];
+                    mapping = {
+                        "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+                        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                        "<C-e>" = "cmp.mapping.abort()";
+                        "<Tab>" = ''
                             function(fallback)
                                 local luasnip = require "luasnip"
                                 if cmp.visible() then cmp.select_next_item()
@@ -136,10 +134,7 @@
                                 end
                             end
                         '';
-                    };
-                    "<S-Tab>" = {
-                        modes = [ "i" "s" ];
-                        action = ''
+                        "<S-Tab>" = ''
                             function(fallback)
                                 local luasnip = require "luasnip"
                                 if cmp.visible() then cmp.select_prev_item()
@@ -148,18 +143,18 @@
                                 end
                             end
                         '';
+                        "<CR>" = ''
+                            cmp.mapping {
+                                i = function(fallback)
+                                    if cmp.visible() and cmp.get_active_entry() then
+                                        cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
+                                    else fallback()
+                                    end
+                                end,
+                                s = cmp.mapping.confirm { select = true }
+                            }
+                        '';
                     };
-                    "<CR>" = ''
-                        cmp.mapping {
-                            i = function(fallback)
-                                if cmp.visible() and cmp.get_active_entry() then
-                                    cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
-                                else fallback()
-                                end
-                            end,
-                            s = cmp.mapping.confirm { select = true }
-                        }
-                    '';
                 };
             };
             trouble.enable = true;
