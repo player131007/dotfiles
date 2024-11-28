7f4912f790ddeb1f5d21f0d3012540727fe188a4
Lương Việt Hoàng <tcm4095@gmail.com>
2024-11-28 10:03:00 +0700

    add stable libvirt

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixpkgs-libvirt.url = "github:NixOS/nixpkgs/4aa36568d413aca0ea84a1684d2d46f55dbabad7";
         impermanence.url = "github:nix-community/impermanence";
         home-manager = {
             url = "github:nix-community/home-manager";
@@ -15,14 +16,15 @@
         base16.url = "github:SenchoPens/base16.nix";
     };
 
-    outputs = { self, nixpkgs, home-manager, impermanence, base16, lix-module }: {
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: {
         nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
             modules = [
-                lix-module.nixosModules.default
-                impermanence.nixosModule
-                base16.nixosModule
+                inputs.lix-module.nixosModules.default
+                inputs.impermanence.nixosModule
+                inputs.base16.nixosModule
                 ./hosts/laptop
                 ./modules/nixos
+                { virtualisation.libvirtd.package = inputs.nixpkgs-libvirt.legacyPackages.x86_64-linux.libvirt; }
             ];
         };
 
@@ -34,7 +36,7 @@
         homeConfigurations."player131007@laptop" = home-manager.lib.homeManagerConfiguration {
             pkgs = self.nixosConfigurations.laptop.pkgs;
             modules = [
-                base16.homeManagerModule
+                inputs.base16.homeManagerModule
                 ./users/player131007
             ];
         };
