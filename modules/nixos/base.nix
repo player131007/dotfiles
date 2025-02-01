9a6d0f77f1998fce2e0651c8fae943fb5bb54432
Lương Việt Hoàng <tcm4095@gmail.com>
2025-02-01 21:06:09 +0700

    refactor common options i'd want in a system

---
+++ modules/nixos/base.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+{
+    nix = {
+        channel.enable = lib.mkDefault false;
+        settings.experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    nixpkgs.config.allowUnfree = lib.mkDefault true;
+
+    boot.kernel.sysctl."kernel.dmesg_restrict" = lib.mkDefault 0;
+}
