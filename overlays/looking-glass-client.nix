fddb75f74b6b01ae77ad1758d5f8217ccea5b6cf
Lương Việt Hoàng <tcm4095@gmail.com>
2025-02-13 09:25:11 +0700

    use normal looking-glass-client

--- overlays/looking-glass-client.nix
+++
@@ -1,7 +0,0 @@
-_: prev: {
-    looking-glass-client = prev.looking-glass-client.overrideAttrs (prevAttrs: {
-        postPatch = prevAttrs.postPatch or "" + ''
-            sed -i 's/"-Wfatal-errors"//g' CMakeLists.txt
-        '';
-    });
-}
