9340c8c79e5f5646c887d790e756a13fec1ceb33
Lương Việt Hoàng <tcm4095@gmail.com>
2025-02-27 12:21:00 +0700

    dont load vfio-pci on startup libvirt can detach devices automatically

--- hosts/laptop/virtualization/win10/default.nix
+++ hosts/laptop/virtualization/win10/default.nix
@@ -1,10 +1,5 @@
 {pkgs, ...}: {
   boot.kernelParams = ["iommu=pt"];
-  boot.kernelModules = ["vfio-pci"];
-
-  boot.extraModprobeConfig = ''
-    options vfio-pci ids=10de:25a2,10de:2291
-  '';
 
   systemd.tmpfiles.rules = [
     "f /dev/shm/looking-glass 0660 player131007 kvm -"
