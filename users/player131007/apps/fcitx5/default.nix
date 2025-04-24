98e37f50c5607e0ca9124417176ca5c7fd189a94
Lương Việt Hoàng <tcm4095@gmail.com>
2025-04-24 12:04:57 +0700

    use new input method options

--- users/player131007/apps/fcitx5/default.nix
+++ users/player131007/apps/fcitx5/default.nix
@@ -4,14 +4,15 @@
   ...
 }: {
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-unikey
     ];
   };
 
   xdg.configFile."fcitx5" = {
-    enable = config.i18n.inputMethod.enabled == "fcitx5";
+    enable = config.i18n.inputMethod.enable == true && config.i18n.inputMethod.type == "fcitx5";
     source = ./.;
   };
 }
