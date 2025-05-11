88f218908bd8995f70ca1e8f6bb9c1181d583f6c
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-12 00:14:01 +0700

    add btop theming

--- users/player131007/apps/btop/default.nix
+++ users/player131007/apps/btop/default.nix
@@ -1,8 +1,24 @@
-{ config, ... }:
+{ config, npins, pkgs, ... }:
 {
+  xdg.configFile."btop/themes/${config.colorscheme.system}-${config.colorscheme.slug}.theme" = {
+    inherit (config.programs.btop) enable;
+    source = let
+      src = npins.tinted-btop;
+      tinted-btop = pkgs.fetchFromGitHub {
+        inherit (src.repository) owner repo;
+        rev = src.revision;
+        sha256 = src.hash;
+      };
+    in config.colorscheme {
+      template = "${tinted-btop}/templates/base16.mustache";
+      extension = "theme";
+    };
+  };
+
   programs.btop = {
     enable = true;
     settings = {
+      color_theme = "${config.colorscheme.system}-${config.colorscheme.slug}";
       truecolor = true;
       theme_background = false;
       update_ms = 1000;
