318b26133ea03c9caa380b4af02067805c986fed
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-12 16:03:37 +0700

    change wallpaper color

--- users/player131007/apps/river/default.nix
+++ users/player131007/apps/river/default.nix
@@ -81,7 +81,7 @@
       };
       spawn = map (s: "\"${s}\"") [
         "rivertile -main-ratio 0.5"
-        "${lib.getExe pkgs.swaybg} -i ${../../wallpaper.png}"
+        "${lib.getExe pkgs.swaybg} -i ${../../wallpaper.png} -m fill"
         "keepassxc --minimized"
       ];
 
