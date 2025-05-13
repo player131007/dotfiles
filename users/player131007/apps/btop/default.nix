0b4d9789436a47713423a6387f3fb23aa97b4ca1
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-14 00:35:46 +0700

    btop proc settings stuff

--- users/player131007/apps/btop/default.nix
+++ users/player131007/apps/btop/default.nix
@@ -17,6 +17,9 @@
       update_ms = 1000;
       disks_filter = "/nix / /boot /d";
       swap_disk = false;
+      proc_tree = true;
+      proc_filter_kernel = true;
+      proc_info_smaps = true;
     };
   };
 }
