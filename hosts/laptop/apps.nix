f7b136178f24d548a8e2c5244178e0e134bb5b5a
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-15 00:23:44 +0700

    add bash shell integration for foot
    thank god i dont have to add it to fish too

--- hosts/laptop/apps.nix
+++ hosts/laptop/apps.nix
@@ -12,12 +12,40 @@
         template = "${npins.tinted-shell}/templates/base24.mustache";
         extension = "sh";
       };
-
-      base24-init = lib.mkAfter "sh ${base24-script}";
     in
     {
-      fish.interactiveShellInit = base24-init;
-      bash.interactiveShellInit = base24-init;
+      fish.interactiveShellInit = lib.mkAfter "sh ${base24-script}";
+      bash.interactiveShellInit = lib.mkAfter ''
+        sh ${base24-script}
+
+        # shell integration
+        osc7_cwd() {
+          local strlen=''${#PWD}
+          local encoded=""
+          local pos c o
+          for (( pos=0; pos<strlen; pos++ )); do
+              c=''${PWD:$pos:1}
+              case "$c" in
+                  [-/:_.!\'\(\)~[:alnum:]] ) o="''${c}" ;;
+                  * ) printf -v o '%%%02X' "''\'''${c}" ;;
+              esac
+              encoded+="''${o}"
+          done
+          printf '\e]7;file://%s%s\e\\' "''${HOSTNAME}" "''${encoded}"
+        }
+        PROMPT_COMMAND=''${PROMPT_COMMAND:+''${PROMPT_COMMAND%;}; }osc7_cwd
+
+        prompt_marker() {
+          printf '\e]133;A\e\\'
+        }
+        PROMPT_COMMAND=''${PROMPT_COMMAND:+$PROMPT_COMMAND; }prompt_marker
+
+        PS0+='\e]133;C\e\\'
+        command_done() {
+            printf '\e]133;D\e\\'
+        }
+        PROMPT_COMMAND=''${PROMPT_COMMAND:+$PROMPT_COMMAND; }command_done
+      '';
       command-not-found.enable = false;
       nix-index.enable = true;
       fish.enable = true;
