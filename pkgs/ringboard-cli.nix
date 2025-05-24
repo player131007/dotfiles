db9892ec26052a534726fd15bfd23a4c03adb097
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-24 22:17:20 +0700

    add ringboard-cli

---
+++ pkgs/ringboard-cli.nix
@@ -0,0 +1,18 @@
+{
+  rustPlatform_nightly,
+  ringboard-server,
+}:
+rustPlatform_nightly.buildRustPackage {
+  pname = "ringboard-cli";
+  inherit (ringboard-server) version src;
+
+  buildAndTestSubdir = "cli";
+
+  cargoHash = "sha256-+E6BzfgUvpBZzkzvPvFfEt/IoVR/wU4uHECs4Dn5pIE=";
+
+  meta = {
+    inherit (ringboard-server.meta) homepage changelog license platforms;
+    description = "CLI for Ringboard, a clipboard manager for Linux";
+    mainProgram = "ringboard";
+  };
+}
