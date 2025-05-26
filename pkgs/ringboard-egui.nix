da2ef2e026ccd962e183f92a6a033839e2c7a6ed
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-26 17:35:12 +0700

    fix ringboard-egui

--- pkgs/ringboard-egui.nix
+++ pkgs/ringboard-egui.nix
@@ -1,8 +1,25 @@
 {
+  lib,
   rustPlatform_nightly,
   ringboard-server,
+
+  autoPatchelfHook,
+  pkg-config,
+
+  stdenv,
+  fontconfig,
+  libxkbcommon,
+  libGL,
+
+  waylandSupport ? true,
+  wayland,
+
+  x11Support ? true,
+  xorg,
 }:
-rustPlatform_nightly.buildRustPackage {
+let
+  inherit (lib) flatten;
+in rustPlatform_nightly.buildRustPackage {
   pname = "ringboard-egui";
   inherit (ringboard-server) version src;
 
@@ -10,11 +27,32 @@ rustPlatform_nightly.buildRustPackage {
 
   cargoHash = "sha256-+E6BzfgUvpBZzkzvPvFfEt/IoVR/wU4uHECs4Dn5pIE=";
 
+  buildInputs = [
+    stdenv.cc.cc.lib
+    fontconfig
+  ];
+  nativeBuildInputs = [
+    autoPatchelfHook
+    pkg-config
+  ];
+
+  buildNoDefaultFeatures = true;
+  buildFeatures = flatten [
+    "system-fonts"
+    (lib.optional waylandSupport "wayland")
+    (lib.optional x11Support "x11")
+  ];
+
+  runtimeDependencies = flatten [
+    libxkbcommon
+    libGL
+    (lib.optionals waylandSupport [ wayland ])
+    (lib.optionals x11Support [ xorg.libX11 xorg.libXcursor xorg.libXi ])
+  ];
+
   meta = {
     inherit (ringboard-server.meta) homepage changelog license platforms;
-    broken = true; # egui
     description = "GUI for Ringboard, a clipboard manager for Linux";
     mainProgram = "ringboard-egui";
   };
 }
-
