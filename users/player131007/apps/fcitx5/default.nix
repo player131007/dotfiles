a3c14e89b8b507ff33b0c7f016a62ef157a061cc
Lương Việt Hoàng <tcm4095@gmail.com>
2025-05-27 17:09:02 +0700

    fcitx5 was broken and now it works again somehow

--- users/player131007/apps/fcitx5/default.nix
+++ users/player131007/apps/fcitx5/default.nix
@@ -7,13 +7,9 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-unikey
-    ];
-  };
-
-  xdg.configFile."fcitx5" = {
-    enable = config.i18n.inputMethod.enable == true && config.i18n.inputMethod.type == "fcitx5";
-    source = ./.;
+    fcitx5 = {
+      addons = [ pkgs.fcitx5-unikey ];
+      waylandFrontend = true;
+    };
   };
 }
