46ca8d104d78ee8b18db3e1a4fcbcbf40045fd41
Lương Việt Hoàng <tcm4095@gmail.com>
2025-06-28 20:42:43 +0700

    fix

--- hosts/unora/hardware-configuration.nix
+++ hosts/unora/hardware-configuration.nix
@@ -61,8 +61,6 @@
   };
 
   boot.initrd.luks.devices.cryptroot = {
-    device = throw "specify your root device bruh";
-    fallbackToPassword = true;
     allowDiscards = true;
     bypassWorkqueues = true;
   };
