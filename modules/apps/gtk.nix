7aaa77cf8394bcf87eec64e36918a83465d9c82f
Lương Việt Hoàng <tcm4095@gmail.com>
2025-07-18 07:49:56 +0700

    add gsettings-desktop-schemas to gsettings-declarative (using glib)

--- modules/apps/gtk.nix
+++ modules/apps/gtk.nix
@@ -1,8 +1,21 @@
+{ inputs, ... }:
 {
   flake.modules.maid.pc =
     { pkgs, ... }:
     {
       packages = [ pkgs.adw-gtk3 ];
+      gsettings.package =
+        let
+          gsettings-declarative = import "${inputs.nix-maid}/gsettings-declarative" { inherit pkgs; };
+        in
+        gsettings-declarative.overrideAttrs (prevAttrs: {
+          nativeBuildInputs = prevAttrs.nativeBuildInputs or [ ] ++ [
+            pkgs.glib
+          ];
+          buildInputs = prevAttrs.buildInputs or [ ] ++ [
+            pkgs.gsettings-desktop-schemas
+          ];
+        });
       gsettings.settings = {
         org.gnome.desktop.interface.gtk-theme = "adw-gtk3";
       };
