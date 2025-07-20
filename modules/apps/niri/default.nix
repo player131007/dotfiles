3c27bb91614dee6d83053e1a90e446ce54479b61
Lương Việt Hoàng <tcm4095@gmail.com>
2025-07-20 14:56:18 +0700

    use nemo as desktop portal file chooser instead

--- modules/apps/niri/default.nix
+++ modules/apps/niri/default.nix
@@ -2,13 +2,23 @@
   flake.modules.nixos.pc = {
     programs.niri.enable = true;
     services.gnome.gnome-keyring.enable = false;
+    xdg.portal.config.niri = {
+      default = [
+        "gnome"
+        "gtk"
+      ];
+      "org.freedesktop.impl.portal.Access" = "gtk";
+      "org.freedesktop.impl.portal.Notification" = "gtk";
+      "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      # no secrets portal
+    };
   };
 
   flake.modules.maid.pc =
     { pkgs, ... }:
     {
       packages = [
-        pkgs.nautilus
+        pkgs.nemo
         pkgs.bibata-cursors
       ];
       file.xdg_config."niri/config.kdl".source = ./config.kdl;
