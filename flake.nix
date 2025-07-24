01b977be984ce57416e43170ed8a4ec3c293a837
Lương Việt Hoàng <tcm4095@gmail.com>
2025-07-24 18:43:44 +0700

    bump lix

--- flake.nix
+++ flake.nix
@@ -21,13 +21,8 @@
     nix-maid.url = "github:viperML/nix-maid";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
-      # FIXME: remove when lix-module updates
-      inputs.lix = {
-        url = "https://git.lix.systems/lix-project/lix/archive/release-2.93.tar.gz";
-        flake = false;
-      };
     };
 
     fenix = {
