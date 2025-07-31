8b5d5d61d3afbf280da1111c031a6d3290e67d24
Lương Việt Hoàng <tcm4095@gmail.com>
2025-07-31 18:55:54 +0700

    refactor systemd-initrd.nix

--- modules/services/systemd-initrd.nix
+++ modules/services/systemd-initrd.nix
@@ -1,7 +1,13 @@
 {
-  # doesnt work on iso images for now
-  # https://github.com/NixOS/nixpkgs/pull/291750
-  flake.modules.nixos.pc = {
+  flake.modules.nixos.base = {
     boot.initrd.systemd.enable = true;
   };
+
+  # doesnt work on iso images for now
+  # https://github.com/NixOS/nixpkgs/pull/291750
+  flake.modules.nixos.iso =
+    { lib, ... }:
+    {
+      boot.initrd.systemd.enable = lib.mkImageMediaOverride false;
+    };
 }
