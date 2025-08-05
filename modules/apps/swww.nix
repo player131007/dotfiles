73b1fea9f3cf7856e6dea003dc3caf78f072ed27
Lương Việt Hoàng <tcm4095@gmail.com>
2025-08-05 16:05:28 +0700

    refactor wallpaper switching

--- modules/apps/swww.nix
+++ modules/apps/swww.nix
@@ -3,15 +3,14 @@
     { pkgs, ... }:
     {
       programs.swww.enable = true;
-      programs.wallust.settings.templates.swww = {
-        template = pkgs.writeShellScript "set-wallpaper" ''
-          exec swww img --transition-type random --transition-fps 120 --transition-duration 1 -- "{{ wallpaper }}"
-        '';
-        target = "~/.cache/set-wallpaper.sh";
+      programs.wallust.settings.templates.wallpaper = {
+        template = pkgs.writeText "wallpaper-template" "{{ wallpaper }}";
+        target = "~/.cache/wallpaper";
       };
       programs.wallust.extraCommands = # bash
         ''
-          bash ~/.cache/set-wallpaper.sh
+          swww img --transition-type random --transition-fps 120 --transition-duration 1 -- "$(<~/.cache/wallpaper)"
+          rm ~/.cache/wallpaper
         '';
     };
 }
