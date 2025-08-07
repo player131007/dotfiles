1563b69c28d4db84bf82698815ee1916bd4c2126
Lương Việt Hoàng <tcm4095@gmail.com>
2025-08-07 08:16:22 +0700

    greetd vt1 only
    https://github.com/NixOS/nixpkgs/commit/431a9761829604e71130790ff6f95e71d4013174

--- modules/services/tuigreet.nix
+++ modules/services/tuigreet.nix
@@ -9,7 +9,6 @@
     {
       services.greetd = {
         enable = true;
-        vt = 2;
         settings.default_session.command = "${lib.getExe pkgs.greetd.tuigreet} -t -d -r --remember-user-session --asterisks --user-menu";
       };
 
