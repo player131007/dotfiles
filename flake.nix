e583138c069f59f357b028017b69a59536afa9dc
Lương Việt Hoàng <tcm4095@gmail.com>
2025-08-07 08:21:27 +0700

    just use lix main at this point

--- flake.nix
+++ flake.nix
@@ -20,9 +20,14 @@
     preservation.url = "github:nix-community/preservation";
     nix-maid.url = "github:viperML/nix-maid";
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     fenix = {
