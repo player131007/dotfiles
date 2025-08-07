94849400adcb7f662b8c25ec3eb8d3bc0d0d3ea6
Lương Việt Hoàng <tcm4095@gmail.com>
2025-08-07 15:46:36 +0700

    change upower critical action

--- modules/services/upower.nix
+++ modules/services/upower.nix
@@ -2,7 +2,10 @@
   flake.modules.nixos.pc =
     { config, ... }:
     {
-      services.upower.enable = true;
+      services.upower = {
+        enable = true;
+        criticalPowerAction = "PowerOff";
+      };
       preservation.preserveAt.${config.stuff.persistDir}.directories = [ "/var/lib/upower" ];
     };
 }
