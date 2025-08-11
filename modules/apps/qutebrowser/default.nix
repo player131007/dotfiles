d25fdc2afa11c2c199a5efd32272495dfb99b5c8
Lương Việt Hoàng <tcm4095@gmail.com>
2025-08-11 15:05:40 +0700

    qutebrowser theme reloading

--- modules/apps/qutebrowser/default.nix
+++ modules/apps/qutebrowser/default.nix
@@ -10,6 +10,10 @@
         template = ./colors.py;
         target = "~/.config/qutebrowser/colors.py";
       };
+      programs.wallust.extraCommands = # bash
+        ''
+          pkill -HUP -f .qutebrowser-wrapped
+        '';
     };
 
   # TODO: fill up config
