7f604f35987a72ba9758d7de1fc92e0c312cd58b
Lương Việt Hoàng <tcm4095@gmail.com>
2025-09-10 17:17:13 +0700

    remove libvirtd hook

--- modules/hosts/unora/libvirtd.nix
+++ modules/hosts/unora/libvirtd.nix
@@ -21,25 +21,6 @@
           runAsRoot = false;
           vhostUserPackages = [ pkgs.virtiofsd ];
         };
-        hooks.qemu = {
-          win11-refresh-drive = pkgs.writeShellScript "win11-refresh-drive" ''
-            set -euo pipefail
-
-            DRIVE="/windows/win11.qcow2"
-            GUEST_NAME="$1"
-            OPERATION="$2"
-            SUB_OPERATION="$3"
-
-            if [ ! -e /windows/a ]; then
-              if [ "$GUEST_NAME" = "win11" -a "$OPERATION" = "prepare" -a "$SUB_OPERATION" = "begin" ]; then
-                qemu-img create -f qcow2 -b ./base.qcow2 -F qcow2 $DRIVE
-                chown qemu-libvirtd $DRIVE
-              elif [ "$GUEST_NAME" = "win11" -a "$OPERATION" = "release" -a "$SUB_OPERATION" = "end" ]; then
-                mv $DRIVE $DRIVE.bak
-              fi
-            fi
-          '';
-        };
       };
 
       preservation.preserveAt.${config.stuff.persistDir}.directories = [ "/var/lib/libvirt" ];
