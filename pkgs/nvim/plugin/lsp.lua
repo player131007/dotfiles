4d1d7deb2c9d6b3537230d3c8f77d165c945c82f
Lương Việt Hoàng <tcm4095@gmail.com>
2025-10-08 10:45:59 +0700

    add lsplog command

--- pkgs/nvim/plugin/lsp.lua
+++ pkgs/nvim/plugin/lsp.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_user_command(
+  "LspLog",
+  function() vim.cmd.tabedit(vim.lsp.log.get_filename()) end,
+  {
+    desc = "Open lsp log file",
+  }
+)
+
 vim.lsp.enable { "emmylua_ls", "nixd" }
 
 -- so treesitter injections can work
