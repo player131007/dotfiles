37f06031b904cc71a7b75b48e6bacf5a313947f3
Lương Việt Hoàng <tcm4095@gmail.com>
2025-11-03 13:32:41 +0700

    default fonts

--- modules/system/pc/fonts.nix
+++ modules/system/pc/fonts.nix
@@ -2,12 +2,32 @@
 {
   fonts.fontconfig = {
     subpixel.rgba = "rgb";
-    defaultFonts = {
-      sansSerif = [ "Inter" ];
-    };
+    defaultFonts =
+      let
+        cjk =
+          font:
+          map (lang: "${font} ${lang}") [
+            "JP"
+            "KR"
+            "SC"
+            "TC"
+            "HK"
+          ];
+      in
+      {
+        serif = cjk "Noto Serif CJK";
+        sansSerif = [ "Inter" ] ++ cjk "Noto Sans CJK";
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [ "Iosevka" ];
+      };
   };
+
   fonts.enableDefaultPackages = false;
   fonts.packages = with pkgs; [
     inter
+    iosevka
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
   ];
 }
