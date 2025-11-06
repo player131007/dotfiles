7446e25419c3e12c0a427c5cafcb0acc8dcb4841
Lương Việt Hoàng <tcm4095@gmail.com>
2025-11-06 12:10:39 +0700

    syncthing

---
+++ modules/system/pc/syncthing.nix
@@ -0,0 +1,63 @@
+{ lib, pkgs, ... }:
+{
+  networking.firewall = {
+    allowedTCPPorts = [ 22000 ];
+    allowedUDPPorts = [
+      22000
+      21027
+    ];
+  };
+
+  systemd.user.services.syncthing = {
+    description = "Syncthing";
+    documentation = [
+      "man:syncthing(1)"
+      "https://docs.syncthing.net/users/syncthing.html"
+    ];
+    wantedBy = [ "default.target" ];
+
+    startLimitBurst = 4;
+    startLimitIntervalSec = 60;
+    environment = {
+      STNOBROWSER = "true";
+      STNORESTART = "true";
+      STNOUPGRADE = "true";
+      STLOGFORMATTIMESTAMP = "";
+      STLOGFORMATLEVELSTRING = "false";
+      STLOGFORMATLEVELSYSLOG = "true";
+      STGUIADDRESS = "https://127.0.0.1:8384";
+    };
+
+    serviceConfig = {
+      ExecStart = "${lib.getExe pkgs.syncthing} serve";
+      Restart = "on-failure";
+      RestartForceExitStatus = "3 4";
+      SuccessExitStatus = "3 4";
+
+      LockPersonality = true;
+      MemoryDenyWriteExecute = true;
+      NoNewPrivileges = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = "@system-service";
+      PrivateDevices = true;
+      PrivateMounts = true;
+      PrivateTmp = true;
+      PrivateUsers = true;
+      ProtectHostname = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      CapabilityBoundingSet = [
+        "~CAP_SYS_PTRACE"
+        "~CAP_SYS_ADMIN"
+        "~CAP_SETGID"
+        "~CAP_SETUID"
+        "~CAP_SETPCAP"
+        "~CAP_SYS_TIME"
+        "~CAP_KILL"
+      ];
+    };
+  };
+}
