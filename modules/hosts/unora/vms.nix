ea15543a115624cb60b4f786871b533aa9351d56
Lương Việt Hoàng <tcm4095@gmail.com>
2025-12-10 17:16:09 +0700

    rewrite libvirt module

--- modules/hosts/unora/vms.nix
+++ modules/hosts/unora/vms.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   username,
   ...
 }:
@@ -22,23 +23,32 @@
   boot.extraModulePackages = [ config.boot.kernelPackages.kvmfr ];
   boot.kernelModules = [ "kvmfr" ];
   boot.extraModprobeConfig = "options kvmfr static_size_mb=32";
-  services.udev.extraRules =
-    let
-      owner = if config.virtualisation.libvirtd.qemu.runAsRoot then "root" else "qemu-libvirtd";
-    in
+  services.udev.packages = lib.singleton (
+    pkgs.writeTextDir "etc/udev/rules.d/70-kvmfr.rules" /* udev */ ''
+      SUBSYSTEM=="kvmfr", GROUP="kvm", MODE="0660", TAG+="uaccess"
     ''
-      SUBSYSTEM=="kvmfr", OWNER="${owner}", GROUP="users", MODE="0660"
-    '';
+  );
 
-  virtualisation.libvirtd.qemu.verbatimConfig = ''
-    cgroup_device_acl = [
-      "/dev/null", "/dev/full", "/dev/zero",
-      "/dev/random", "/dev/urandom",
-      "/dev/ptmx", "/dev/kvm",
-      "/dev/userfaultfd",
-      "/dev/kvmfr0"
-    ]
-  '';
+  virtualisation.libvirtd.qemu.verbatimConfig =
+    let
+      kvmfr_max_devices = 10; # upstream
+      kvmfr_devices = builtins.genList (i: "/dev/kvmfr${toString i}") kvmfr_max_devices;
+
+      cgroup_device_acl =
+        # libvirt defaults
+        [
+          "/dev/null"
+          "/dev/full"
+          "/dev/zero"
+          "/dev/random"
+          "/dev/urandom"
+          "/dev/ptmx"
+          "/dev/kvm"
+          "/dev/userfaultfd"
+        ]
+        ++ kvmfr_devices;
+    in
+    "cgroup_device_acl = ${builtins.toJSON cgroup_device_acl}";
 
   my.hjem = {
     packages = [ pkgs.looking-glass-client ];
