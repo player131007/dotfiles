00e57e0522f4ebf8c703d018f3763926d4f3e719
Lương Việt Hoàng <tcm4095@gmail.com>
2025-12-16 22:03:12 +0700

    remove lsp stuff

--- pkgs/nvim/plugin/lsp.lua
+++ pkgs/nvim/plugin/lsp.lua
@@ -1,12 +1,4 @@
-vim.api.nvim_create_user_command(
-  "LspLog",
-  function() vim.cmd.tabedit(vim.lsp.log.get_filename()) end,
-  {
-    desc = "Open lsp log file",
-  }
-)
+-- semantic tokens override treesitter injections
+vim.api.nvim_set_hl(0, "@lsp.type.string.lua", {})
 
 vim.lsp.enable { "emmylua_ls", "nixd" }
-
--- so treesitter injections can work
-vim.api.nvim_set_hl(0, "@lsp.type.string.lua", {})
