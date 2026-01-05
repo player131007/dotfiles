9ee3713e1ab4ae8ca7e10050c4fafe9c992afc71
Lương Việt Hoàng <tcm4095@gmail.com>
2026-01-05 12:07:47 +0700

    disable redefined local hint

--- pkgs/nvim/.nvim/lsp/emmylua_ls.lua
+++ pkgs/nvim/.nvim/lsp/emmylua_ls.lua
@@ -1,7 +1,11 @@
+---@diagnostic disable-next-line: missing-fields
 ---@type vim.lsp.ClientConfig
 return {
   settings = {
     emmylua = {
+      diagnostics = {
+        disable = { "redefined-local" },
+      },
       runtime = {
         version = "LuaJIT",
       },
