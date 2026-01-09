74e1275e5aa2cdc5ac4a2e6864c22736a72aea9c
Lương Việt Hoàng <tcm4095@gmail.com>
2026-01-09 22:05:19 +0700

    enable tlp

--- modules/system/pc/power.nix
+++ modules/system/pc/power.nix
@@ -5,8 +5,12 @@
   };
   persist.at.persistdir.directories = [ "/var/lib/upower" ];
 
-  services.power-profiles-daemon.enable = true;
-  persist.at.oncedir.directories = [ "/var/lib/power-profiles-daemon" ];
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    pd.enable = true;
+  };
+  persist.at.oncedir.directories = [ "/var/lib/tlp" ];
 
   services.logind.settings.Login = {
     HandleLidSwitch = "ignore";
