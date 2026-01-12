3df87a6e5c074c6d20b3545c903fd723f21a2d96
Lương Việt Hoàng <tcm4095@gmail.com>
2026-01-12 11:04:21 +0700

    add youtube ad block script

--- modules/programs/extras/qutebrowser/default.nix
+++ modules/programs/extras/qutebrowser/default.nix
@@ -7,12 +7,21 @@
 
   my.hjem = {
     packages = [ pkgs.qutebrowser ];
-    xdg.config.files."qutebrowser/config.py" = {
-      enable = true;
-      type = "copy";
-      permissions = "600";
-      clobber = true;
-      text = builtins.readFile ./config.py;
+    xdg.config.files = {
+      "qutebrowser/config.py" = {
+        enable = true;
+        type = "copy";
+        permissions = "600";
+        clobber = true;
+        text = builtins.readFile ./config.py;
+      };
+      "qutebrowser/greasemonkey/yt-ads.js" = {
+        enable = true;
+        type = "copy";
+        permissions = "600";
+        clobber = true;
+        source = ./yt-ads.js;
+      };
     };
   };
 }
