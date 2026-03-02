53aa71c55daee3938199eab8aabedae370c1fb23
Lương Việt Hoàng <tcm4095@gmail.com>
2026-03-02 19:55:16 +0700

    remove fenix

--- packages.nix
+++ packages.nix
@@ -7,22 +7,10 @@ in
 let
   inherit (pkgs) lib;
 
-  fenix = import sources.fenix {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    inherit pkgs lib;
-  };
   mnw = import sources.mnw;
-
-  toolchain = fenix.minimal.toolchain;
-  scope = lib.makeScope pkgs.newScope (_: {
-    rustPlatform_nightly = pkgs.makeRustPlatform {
-      rustc = toolchain;
-      cargo = toolchain;
-    };
-  });
 in
 lib.packagesFromDirectoryRecursive {
-  inherit (scope) callPackage newScope;
+  inherit (pkgs) callPackage newScope;
   directory = ./pkgs/by-name;
 }
 // {
