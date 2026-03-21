5a45fd28aca7d1f153048fb543486688a4da0fd6
Lương Việt Hoàng <tcm4095@gmail.com>
2026-03-21 22:48:30 +0700

    rewrite skip youtube ads userscript

--- modules/programs/qutebrowser/yt-ads.js
+++ modules/programs/qutebrowser/yt-ads.js
@@ -1,47 +1,63 @@
 // ==UserScript==
 // for more updated scripts, see: https://greasyfork.org/en/scripts/by-site/youtube.com
 // @name         Auto Skip YouTube Ads
-// @version      1.1.0
 // @description  Speed up and skip YouTube ads automatically
-// @author       jso8910 and others
 // @match        *://*.youtube.com/*
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
-
 (function() {
-  'use strict'
+  "use strict";
 
-  document.addEventListener('load', () => {
-    const adShowing = document.querySelector('.ad-showing');
-    if (adShowing) {
-      const ad = document.querySelector('video');
-      ad.play()
+  const style = document.createElement("style");
+  style.id = "noads"
+  style.textContent = `
+    #player-ads,
+    #panels > ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"],
+    #masthead-ad,
+    .yt-mealbar-promo-renderer,
+    .ytp-featured-product,
+    ytd-merch-shelf-renderer,
+    ytmusic-mealbar-promo-renderer,
+    ytmusic-statement-banner-renderer,
+    ytd-reel-video-renderer:has(.ytd-ad-slot-renderer),
+    ytd-rich-item-renderer:has(.ytd-ad-slot-renderer),
+    ytd-ad-slot-renderer {
+      display: none !important;
+    }
+  `;
+  document.head.appendChild(style);
+
+  function check_and_skip_ads(player) {
+    if(player.classList.contains("ad-showing")) {
+      console.log("found ad");
+      const ad = document.getElementsByClassName("html5-main-video")[0];
+
       ad.currentTime = 9999999999;
-      setTimeout(() => { ad.currentTime = 0; }, 100)
-    }
-  }, true);
+      ad.play().then(() => { ad.currentTime = 0; });
 
-  document.addEventListener('readystatechange', () => {
-    if (document.readyState == 'interactive') {
-      const style = document.createElement('style');
-      style.id = "noads"
-      style.textContent =
-`
-#player-ads,
-#panels > ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"],
-#masthead-ad,
-.yt-mealbar-promo-renderer,
-.ytp-featured-product,
-ytd-merch-shelf-renderer,
-ytmusic-mealbar-promo-renderer,
-ytmusic-statement-banner-renderer,
-ytd-reel-video-renderer:has(.ytd-ad-slot-renderer),
-ytd-rich-item-renderer:has(.ytd-ad-slot-renderer),
-ytd-ad-slot-renderer
-{ display: none !important; }
-`;
-      document.head.appendChild(style);
+      return true;
     }
+
+    return false;
+  }
+
+  const adObserver = new MutationObserver((mutations, observer) => {
+    for(const m of mutations) if(check_and_skip_ads(m.target)) {
+      observer.takeRecords();
+      return;
+    }
+  });
+
+  document.addEventListener("yt-navigate-finish", () => {
+    const player = document.getElementById("movie_player");
+    if(player) {
+      check_and_skip_ads(player);
+      adObserver.observe(player, { attributeFilter: [ "class" ] });
+    }
+  });
+
+  document.addEventListener("yt-navigate-start", () => {
+    adObserver.disconnect();
   });
 })();
