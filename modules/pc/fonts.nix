326781b786b553b3968e1a9fc7372d53da6d5cbb
Lương Việt Hoàng <tcm4095@gmail.com>
2026-03-26 11:06:54 +0700

    change installed fonts

--- modules/pc/fonts.nix
+++ modules/pc/fonts.nix
@@ -1,25 +1,14 @@
 { pkgs, ... }:
 {
+  fonts.enableDefaultPackages = false;
+
   fonts.fontconfig = {
     subpixel.rgba = "rgb";
-    defaultFonts =
-      let
-        cjk =
-          font:
-          map (lang: "${font} ${lang}") [
-            "JP"
-            "KR"
-            "SC"
-            "TC"
-            "HK"
-          ];
-      in
-      {
-        serif = cjk "Noto Serif CJK";
-        sansSerif = [ "Inter" ] ++ cjk "Noto Sans CJK";
-        emoji = [ "Noto Color Emoji" ];
-        monospace = [ "Iosevka" ];
-      };
+    defaultFonts = {
+      sansSerif = [ "Inter" ];
+      emoji = [ "Noto Color Emoji" ];
+      monospace = [ "Iosevka" ];
+    };
 
     localConf = /* xml */ ''
       <?xml version='1.0'?>
@@ -40,7 +29,10 @@
   fonts.packages = with pkgs; [
     inter
     iosevka
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
+
+    freefont_ttf
+    gyre-fonts
+    unifont
+    noto-fonts-color-emoji
   ];
 }
