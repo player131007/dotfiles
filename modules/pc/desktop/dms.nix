48bcd009838fba27b37e34906f7ff984088d9c53
Lương Việt Hoàng <tcm4095@gmail.com>
2026-03-30 15:14:15 +0700

    dont theme stuff with dms

--- modules/pc/desktop/dms.nix
+++ modules/pc/desktop/dms.nix
@@ -11,29 +11,23 @@
     enableAudioWavelength = false;
     enableCalendarEvents = false;
     enableClipboardPaste = false;
+    enableDynamicTheming = false;
     enableVPN = false;
   };
 
   services.accounts-daemon.enable = true;
   persist.at.oncedir.directories = [ "/var/lib/AccountsService" ];
 
-  my.hjem =
-    { config, ... }:
-    {
-      xdg.config.files = {
-        "niri/config.kdl" = {
-          enable = lib.mkDefault false;
-          text = lib.mkAfter /* kdl */ ''
-            include "dms/colors.kdl"
-            include "dms/cursor.kdl"
-            include "dms/wpblur.kdl"
-          '';
-        };
-
-        "foot/foot.ini" = {
-          enable = lib.mkDefault false;
-          value.main.include = [ "${config.xdg.config.directory}/foot/dank-colors.ini" ];
-        };
+  my.hjem = {
+    xdg.config.files = {
+      "niri/config.kdl" = {
+        enable = lib.mkDefault false;
+        text = lib.mkAfter /* kdl */ ''
+          include "dms/colors.kdl"
+          include "dms/cursor.kdl"
+          include "dms/wpblur.kdl"
+        '';
       };
     };
+  };
 }
