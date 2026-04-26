8b2e1d1b47730a8fe1eca03e0e2d320764db1840
Lương Việt Hoàng <tcm4095@gmail.com>
2026-04-26 12:03:04 +0700

    update bexpand

--- pkgs/by-name/nushellPlugins/bexpand.nix
+++ pkgs/by-name/nushellPlugins/bexpand.nix
@@ -6,22 +6,22 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "nu_plugin_bexpand";
-  version = "1.3.11100";
+  version = "1.3.11211+nu-0.112.1";
 
   src = fetchFromGitea {
     domain = "forge.axfive.net";
     owner = "Taylor";
     repo = "nu-plugin-bexpand";
-    rev = "5d316cd6620004214c16b41a0912efdc5e6a72db";
-    hash = "sha256-3kCXtXGgUTAVu0qc+TjxbWQFaF2yJo2fKWYadH5+/oQ=";
+    rev = "aab59283343e2d06d119311abf5e8c028c7579e8";
+    hash = "sha256-aO3SL7DO6932nW0kVRtYoWCtzR8fzGAZ5mBdF++GinY=";
   };
 
-  cargoHash = "sha256-FkOExHoSrEC3d8CTxEAGBFZ4qyXAn4+9xv/iydVO3Uw=";
+  cargoHash = "sha256-cgkgNb2Rb+6568TiREBakuEAdAEUgZUvWIi6N3SVPrM=";
 
   meta = {
     description = "Bash style brace expansion for nushell";
     mainProgram = "nu_plugin_bexpand";
 
-    broken = lib.versions.majorMinor nushell.version != "0.111";
+    broken = lib.versions.majorMinor nushell.version != "0.112";
   };
 }
