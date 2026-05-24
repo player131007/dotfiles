420feaf79e2f7c7e9088861f5877e56e493d8276
Lương Việt Hoàng <tcm4095@gmail.com>
2026-05-24 18:18:28 +0700

    kmscon module bug workaround

--- hosts/tahari/kmscon.nix
+++ hosts/tahari/kmscon.nix
@@ -1,3 +1,11 @@
 {
   services.kmscon.extraConfig = "font-engine=unifont";
+
+  # fix bug in kmscon module
+  systemd.services."getty.target".enable = false;
+  systemd.targets.getty.wants = [ "kmsconvt@tty1.service" ];
+  systemd.additionalUpstreamSystemUnits = [
+    "getty.target"
+    "getty-pre.target"
+  ];
 }
