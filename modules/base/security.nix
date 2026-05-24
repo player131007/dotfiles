a3aba631773a01c4025dbfb474849e1d3411c92e
Lương Việt Hoàng <tcm4095@gmail.com>
2026-05-24 19:07:29 +0700

    remove redundant polkit rule

--- modules/base/security.nix
+++ modules/base/security.nix
@@ -4,13 +4,6 @@
     polkit = {
       enable = true;
       debug = true;
-      extraConfig = ''
-        polkit.addRule(function(action, subject) {
-          if (action.id == "org.freedesktop.systemd1.manage-units") {
-            return polkit.Result.AUTH_ADMIN_KEEP;
-          }
-        });
-      '';
     };
   };
 
