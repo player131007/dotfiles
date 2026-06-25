c173157da88bad116ee586e5dbfa58120aaab43d
Lương Việt Hoàng <tcm4095@gmail.com>
2026-06-25 21:14:24 +0700

    fix carapace bash

--- modules/programs/carapace.nix
+++ modules/programs/carapace.nix
@@ -1,6 +1,50 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 let
-  carapace = lib.getExe pkgs.carapace;
+  carapace-nu = pkgs.writeTextDir "share/nushell/vendor/autoload/carapace.nu" ''
+    let bin = ($env.XDG_CONFIG_HOME? | default ([ $env.HOME ".config" ] | path join)) | path join "carapace/bin"
+    $env.PATH = $env.PATH | prepend $bin
+
+    load-env {
+      CARAPACE_SHELL_BUILTINS: (help commands | where category != "" | get name | each { split row " " | first } | uniq | str join "\n")
+      CARAPACE_SHELL_FUNCTIONS: (help commands | where category == "" | get name | each { split row " " | first } | uniq | str join "\n")
+    }
+
+    let fish_completer = {|spans|
+      fish --command $"complete '--do-complete=($spans | str replace --all "'" "\\'" | str join ' ')'"
+      | from tsv --flexible --noheaders --no-infer
+      | rename value description
+      | update value {|row|
+        let value = $row.value
+        let need_quote = ['\' ',' '[' ']' '(' ')' ' ' '\t' "'" '"' "`"] | any {$in in $value}
+        if ($need_quote and ($value | path exists)) {
+          let expanded_path = if ($value starts-with ~) {$value | path expand --no-symlink} else {$value}
+          $'"($expanded_path | str replace --all "\"" "\\\"")"'
+        } else {$value}
+      }
+    }
+
+    let carapace_completer = {|spans|
+      carapace $spans.0 nushell ...$spans | from json
+    }
+
+    $env.config.completions.external = {
+      enable: true
+      max_results: 50
+      completer: {|spans|
+        if ($spans.0 == "nix"
+            and $spans.1 in [ "shell" "develop" ]
+            and ("-c" in $spans or "--command" in $spans)
+           ) {
+          $spans | skip until {|arg| $arg in ["-c" "--command"]} | skip
+        } else $spans | let $spans
+
+        match $spans.0 {
+          git | nix => $fish_completer
+          _ => $carapace_completer
+        } | do $in $spans
+      }
+    }
+  '';
 in
 {
   environment.sessionVariables = {
@@ -8,56 +52,12 @@ in
     CARAPACE_LENIENT = "1";
     CARAPACE_EXCLUDES = "nix,git";
   };
-
   programs.bash.interactiveShellInit = /* bash */ ''
-    source <(${carapace} _carapace bash)
+    eval $(carapace _carapace bash)
   '';
 
-  environment.systemPackages = lib.singleton (
-    pkgs.writeTextDir "share/nushell/vendor/autoload/carapace.nu" ''
-      let bin = ($env.XDG_CONFIG_HOME? | default ([ $env.HOME ".config" ] | path join)) | path join "carapace/bin"
-      $env.PATH = $env.PATH | prepend $bin
-
-      load-env {
-        CARAPACE_SHELL_BUILTINS: (help commands | where category != "" | get name | each { split row " " | first } | uniq | str join "\n")
-        CARAPACE_SHELL_FUNCTIONS: (help commands | where category == "" | get name | each { split row " " | first } | uniq | str join "\n")
-      }
-
-      let fish_completer = {|spans|
-        fish --command $"complete '--do-complete=($spans | str replace --all "'" "\\'" | str join ' ')'"
-        | from tsv --flexible --noheaders --no-infer
-        | rename value description
-        | update value {|row|
-          let value = $row.value
-          let need_quote = ['\' ',' '[' ']' '(' ')' ' ' '\t' "'" '"' "`"] | any {$in in $value}
-          if ($need_quote and ($value | path exists)) {
-            let expanded_path = if ($value starts-with ~) {$value | path expand --no-symlink} else {$value}
-            $'"($expanded_path | str replace --all "\"" "\\\"")"'
-          } else {$value}
-        }
-      }
-
-      let carapace_completer = {|spans|
-        ${carapace} $spans.0 nushell ...$spans | from json
-      }
-
-      $env.config.completions.external = {
-        enable: true
-        max_results: 50
-        completer: {|spans|
-          if ($spans.0 == "nix"
-              and $spans.1 in [ "shell" "develop" ]
-              and ("-c" in $spans or "--command" in $spans)
-             ) {
-            $spans | skip until {|arg| $arg in ["-c" "--command"]} | skip
-          } else $spans | let $spans
-
-          match $spans.0 {
-            git | nix => $fish_completer
-            _ => $carapace_completer
-          } | do $in $spans
-        }
-      }
-    ''
-  );
+  environment.systemPackages = [
+    pkgs.carapace
+    carapace-nu
+  ];
 }
