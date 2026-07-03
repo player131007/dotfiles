56897c284856da68ef36c8e9e2064a450d4fef9a
Lương Việt Hoàng <tcm4095@gmail.com>
2026-07-04 00:03:32 +0700

    make polkit log info

--- modules/base/security.nix
+++ modules/base/security.nix
@@ -3,7 +3,7 @@
     sudo.enable = false; # replaced by run0
     polkit = {
       enable = true;
-      extraArgs = [ "--log-level=notice" ];
+      extraArgs = [ "--log-level=info" ];
     };
   };
 
