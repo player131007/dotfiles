194f5b3cec0a65de5ad75a47122be25288d973b2
Lương Việt Hoàng <tcm4095@gmail.com>
2026-07-07 07:40:09 +0700

    remove explicitly set `commonMountOptions`

--- modules/pc/persistence.nix
+++ modules/pc/persistence.nix
@@ -20,60 +20,49 @@
   };
 
   persist = lib.mkMerge [
-    (
-      let
-        commonMountOptions = [
-          "x-gvfs-hide"
-          "x-gdu.hide"
-          "X-fstrim.notrim"
-        ];
-      in
-      {
-        tmpfilesSettings.initrd = {
-          "/sysroot/persist/once/etc/machine-id".f = {
-            mode = "0444";
-            user = "root";
-            group = "root";
-            argument = "uninitialized";
-          };
+    {
+      tmpfilesSettings.initrd = {
+        "/sysroot/persist/once/etc/machine-id".f = {
+          mode = "0444";
+          user = "root";
+          group = "root";
+          argument = "uninitialized";
         };
+      };
 
-        at.oncedir = {
-          inherit commonMountOptions;
-          storagePath = "/persist/once";
-          directories = [
-            "/var/lib/nixos"
-            {
-              directory = "/var/lib/systemd";
-              early = true;
-            }
-            {
-              directory = "/var/log";
-              method.bindmount.extraConfig = {
+      at.oncedir = {
+        storagePath = "/persist/once";
+        directories = [
+          "/var/lib/nixos"
+          {
+            directory = "/var/lib/systemd";
+            early = true;
+          }
+          {
+            directory = "/var/log";
+            method.bindmount.extraConfig = {
+              conflicts = lib.mkForce [ ];
+              before = lib.mkForce [ "persistence.target" ];
+            };
+          }
+          {
+            directory = "/nix";
+            early = true;
+            method.bindmount = {
+              mountOptions = [ "slave" ];
+              extraConfig = {
                 conflicts = lib.mkForce [ ];
                 before = lib.mkForce [ "persistence.target" ];
               };
-            }
-            {
-              directory = "/nix";
-              early = true;
-              method.bindmount = {
-                mountOptions = [ "slave" ];
-                extraConfig = {
-                  conflicts = lib.mkForce [ ];
-                  before = lib.mkForce [ "persistence.target" ];
-                };
-              };
-            }
-          ];
-        };
+            };
+          }
+        ];
+      };
 
-        at.persistdir = {
-          inherit commonMountOptions;
-          storagePath = "/persist/every";
-        };
-      }
-    )
+      at.persistdir = {
+        storagePath = "/persist/every";
+      };
+    }
 
     # i don't know where to put this
     (lib.mkIf config.networking.wireless.iwd.enable {
