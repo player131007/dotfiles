ac6bbac9020c8295b89819af918cef2aefd87681
Lương Việt Hoàng <tcm4095@gmail.com>
2026-07-12 06:02:46 +0700

    remove vendored getty module

--- modules/base/kmscon.nix
+++ modules/base/kmscon.nix
@@ -1,13 +1,4 @@
-{ myLib, ... }:
 {
-  disabledModules = [
-    "services/ttys/getty.nix"
-  ];
-
-  imports = [
-    (myLib.fromRoot "modules/delete_me/getty.nix")
-  ];
-
   console.enable = false;
   services.kmscon = {
     enable = true;
