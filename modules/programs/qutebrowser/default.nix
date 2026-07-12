cdaba5c340dd541381d57a520a7fc1cb1972c0d2
Lương Việt Hoàng <tcm4095@gmail.com>
2026-07-12 18:02:27 +0700

    pin qutebrowser because of bug

--- modules/programs/qutebrowser/default.nix
+++ modules/programs/qutebrowser/default.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
+let
+  # FIXME: remove
+  oldPkgs = import (fetchTarball {
+    url = "https://releases.nixos.org/nixos/unstable/nixos-26.11pre1017464.567a49d1913c/nixexprs.tar.xz";
+    sha256 = "sha256-rcUHdUtJEvMdNEl2Wq+YpHraHKfcer3KsBscpZEF2Yg=";
+  }) { };
+in
 {
   fonts.packages = [
     pkgs.inter
   ];
 
   my.hjem = {
-    packages = [ pkgs.qutebrowser ];
+    packages = [ oldPkgs.qutebrowser ];
     xdg.config.files = {
       "qutebrowser/config.py" = {
         enable = true;
