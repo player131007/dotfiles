6bdf4faa1853369101594f1f890e346606d1acff
Lương Việt Hoàng <tcm4095@gmail.com>
2026-07-20 06:45:19 +0700

    use resolved dns directly

--- modules/base/network.nix
+++ modules/base/network.nix
@@ -42,17 +42,15 @@
   services.resolved = {
     enable = true;
     settings.Resolve = {
-      Domains = [ "~." ];
+      DNS = [
+        "1.1.1.1#cloudflare-dns.com"
+        "2606:4700:4700::1111#cloudflare-dns.com"
+        "9.9.9.9#dns.quad9.net"
+        "2620:fe::9#dns.quad9.net"
+        "8.8.8.8#dns.google"
+        "2001:4860:4860::8888#dns.google"
+      ];
       DNSOverTLS = true;
     };
   };
-
-  networking.nameservers = [
-    "1.1.1.1#cloudflare-dns.com"
-    "2606:4700:4700::1111#cloudflare-dns.com"
-    "9.9.9.9#dns.quad9.net"
-    "2620:fe::9#dns.quad9.net"
-    "8.8.8.8#dns.google"
-    "2001:4860:4860::8888#dns.google"
-  ];
 }
