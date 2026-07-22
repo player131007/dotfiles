abf8a05e33434c5eb70cef0df4d64737961df026
avery <tcm4095@gmail.com>
2026-07-22 07:00:59 +0700

    only add nixpkgs to nix_path if it has the store prefix
    because in `nixos-install` it has the path `/mnt/nix/store/...-source`

--- modules/base/nixpkgs.nix
+++ modules/base/nixpkgs.nix
@@ -1,13 +1,8 @@
 { lib, pkgs, ... }:
 {
-  nix =
+  nix = lib.mkIf (lib.path.hasStorePathPrefix pkgs.path) (
     let
-      pkgs_path =
-        if lib.path.hasStorePathPrefix pkgs.path then
-          builtins.storePath pkgs.path
-        else
-          # bailing out, you're on your own.
-          toString pkgs.path;
+      pkgs_path = builtins.storePath pkgs.path;
     in
     {
       registry.nixpkgs.to = {
@@ -15,7 +10,8 @@
         path = pkgs_path;
       };
       nixPath = [ "nixpkgs=${pkgs_path}" ];
-    };
+    }
+  );
 
   nixpkgs = {
     config.allowUnfree = true;
