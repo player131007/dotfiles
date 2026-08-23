9669e52ea344236d2a27806622d9aecc7d50ba9f
avery <tcm4095@gmail.com>
2026-08-23 09:21:15 +0700

    configure tuigreet with config file

--- modules/pc/services/greetd.nix
+++ modules/pc/services/greetd.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   ...
@@ -7,11 +8,29 @@
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = "${lib.getExe pkgs.tuigreet} -t -d -r --remember-user-session --asterisks --user-menu";
+      default_session.command = lib.getExe pkgs.tuigreet;
       general.source_profile = true;
     };
     useTextGreeter = true;
   };
+
+  environment.etc."tuigreet/config.toml".source =
+    let
+      format = pkgs.formats.toml { };
+    in
+    format.generate "tuigreet-config.toml" {
+      general.debug = true;
+      display.show_time = true;
+      remember = {
+        username = true;
+        user_session = true;
+      };
+
+      secret = {
+        mode = "characters";
+        characters = "*";
+      };
+    };
 
   persist.at.oncedir.directories = lib.singleton {
     directory = "/var/cache/tuigreet";
